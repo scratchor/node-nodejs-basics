26fa39a4eab06bfd579589eac1ebc64ddcc4e49f
utsarapkin <utsarapkin@fnc-group.ru>
2022-06-05 22:28:06 +0300

    feat: implement hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,26 @@
+const { createHash } = await import('crypto');
+import { createReadStream } from 'fs';
+import path, {dirname} from "path";
+import {fileURLToPath} from "url";
+import {pipeline} from "stream";
+
+const folderName = 'files'
+const fileName = 'fileToCalculateHashFor.txt'
+
 export const calculateHash = async () => {
-    // Write your code here 
+    try {
+        const filePath = path.join(dirname(fileURLToPath(import.meta.url)), folderName, fileName);
+        const hash = createHash('sha256');
+        const inputFileStream = createReadStream(filePath);
+
+        pipeline(inputFileStream, hash.setEncoding('hex'), process.stdout,(err) => {
+            if (err) {
+                throw err;
+            }
+        })
+    } catch (err) {
+        console.error(err)
+    }
 };
+
+calculateHash();
