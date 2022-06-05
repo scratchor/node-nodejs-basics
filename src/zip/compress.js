e48c45d8ecd005829475388a996c990f7672311f
utsarapkin <utsarapkin@fnc-group.ru>
2022-06-05 22:29:45 +0300

    feat: implement zlib tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,25 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from "stream/promises";
+import { createGzip } from "zlib";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+
+const folderName = 'files'
+const sourceFile = 'fileToCompress.txt'
+const archiveFile = 'archive.gz'
+
 export const compress = async () => {
-    // Write your code here 
+    try {
+        const folderPath = path.join(dirname(fileURLToPath(import.meta.url)), folderName);
+        const gzipTransformStream = createGzip();
+        const readStream = createReadStream(path.join(folderPath, sourceFile));
+        const writeStream = createWriteStream(path.join(folderPath, archiveFile));
+
+       await pipeline(readStream, gzipTransformStream, writeStream)
+    } catch (err) {
+        console.error(err)
+    }
 };
+
+compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,25 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from "stream/promises";
+import { createGunzip } from "zlib";
+import path, {dirname} from "path";
+import {fileURLToPath} from "url";
+
+
+const folderName = 'files'
+const archiveFile = 'archive.gz'
+const destinationFile = 'fileToCompress.txt'
+
 export const decompress = async () => {
-    // Write your code here 
+    try {
+        const folderPath = path.join(dirname(fileURLToPath(import.meta.url)), folderName);
+        const gunzipTransformStream = createGunzip();
+        const readStream = createReadStream(path.join(folderPath, archiveFile));
+        const writeStream = createWriteStream(path.join(folderPath, destinationFile));
+
+        await pipeline(readStream, gunzipTransformStream, writeStream);
+    } catch (err) {
+        console.error(err)
+    }
 };
+
+decompress();
