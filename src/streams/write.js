23a8cfb3cac4852b6c9e88b9b3dd82f6d8b4e3cb
utsarapkin <utsarapkin@fnc-group.ru>
2022-06-05 22:28:30 +0300

    feat: implement stream tasks

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,20 @@
+import {fileURLToPath} from "url";
+import { dirname } from 'path';
+import { createWriteStream } from 'fs';
+import * as path from "path";
+
+const folderName = 'files'
+const fileName = 'fileToWrite.txt'
+
 export const write = async () => {
-    // Write your code here 
+    const __dirname = dirname(fileURLToPath(import.meta.url))
+    const writeStream = createWriteStream(path.join(__dirname, folderName, fileName));
+
+    writeStream.on('error', (err) => {
+        throw err
+    })
+
+    process.stdin.pipe(writeStream);
 };
+
+write()
