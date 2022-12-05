b9ba3c94743ef720882c2a654d837199d6431000
utsarapkin <utsarapkin@fnc-group.ru>
2022-12-05 20:03:21 +0300

    feat: implement fs functions

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,20 @@
+import { readdir } from 'fs/promises';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const folderName = 'files';
+
 export const list = async () => {
-    // Write your code here 
+    try {
+        const __dirname = dirname(fileURLToPath(import.meta.url));
+        const files = await readdir(path.join(__dirname, folderName));
+
+        for (const file of files) {
+            console.log(file);
+        }
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
+
+list();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,37 @@
-export const rename = async () => {
-    // Write your code here 
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { access } from 'fs/promises';
+import { rename as renameMethod } from 'fs';
+
+const folderName = 'files';
+const oldName = 'wrongFilename.txt';
+const newName = 'properFilename.md';
+
+const checkIsFileExist = async (path) => {
+    try {
+        await access(path);
+        return true;
+    } catch (err) {
+        return false;
+    }
 };
+
+export const rename = async () => {
+    try {
+        const folderPath = path.join(dirname(fileURLToPath(import.meta.url)), folderName);
+        const isFileExist = await checkIsFileExist(path.join(folderPath, newName));
+
+        if (isFileExist) {
+            throw new Error('FS operation failed');
+        }
+
+        renameMethod(path.join(folderPath, oldName), path.join(folderPath, newName), (err) => {
+            if (err) throw new Error('FS operation failed');
+        });
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+rename();
