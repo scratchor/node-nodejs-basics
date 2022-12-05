a8692bc0b202bceebaea5c78553da30b89e32874
utsarapkin <utsarapkin@fnc-group.ru>
2022-12-05 22:20:32 +0300

    feat: implement worker threads task

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,6 +1,18 @@
+import { parentPort, workerData } from 'worker_threads';
+
 // n should be received from main thread
 export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 export const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+    try {
+        parentPort.postMessage(nthFibonacci(workerData.num));
+    } catch (err) {
+        if (!parentPort) {
+            console.error('Probably, there is no main thread!');
+        } else {
+            console.error(err);
+        }
+    }
 };
+
+sendResult();
