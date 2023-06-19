277c6b0cd58635e80c71be7cdd86e32f84b3216d
utsarapkin <utsarapkin@fnc-group.ru>
2023-06-20 01:00:18 +0300

    feat: implement cli functions

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,20 @@
 const parseEnv = () => {
-    // Write your code here 
+  try {
+    let string = "";
+    const variables = Object.keys(process.env).filter((e) => e.match(/RSS_/));
+
+    variables.forEach((e, i, arr) => {
+      string += `${e}=${process.env[e]}`;
+
+      if (!(i === arr.length - 1)) {
+        string += "; ";
+      }
+    });
+
+    console.log(string);
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 parseEnv();
